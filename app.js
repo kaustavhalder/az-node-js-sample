f500c638593fd68d15109a82704cc4ffda167397
kaustavhalder <khalder.08@gmail.com>
2020-12-03 12:00:53 +0530

    Updated root URL

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-	res.send('Hello World!')
+	res.send('Hello Wo231312312312312rld!')
 })
 
 app.get('/api/health', (req, res) => {
