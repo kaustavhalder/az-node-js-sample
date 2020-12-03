ffc3089c8d42104105078e0215161577039bf3b9
kaustavhalder <khalder.08@gmail.com>
2020-12-03 11:50:41 +0530

    Added Health Route

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
 	res.send('Hello World!')
 })
 
-app.get('/health', (req, res) => {
+app.get('/api/health', (req, res) => {
 	res.send('App Service is UP')
 })
 app.listen(port, () => {
